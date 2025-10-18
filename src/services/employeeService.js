245e522b7971a35083188b891a64102840413103
Rahul Podicheti <your_email@example.com>
2025-10-18 15:57:18 +0530

    updated

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/employees";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getEmployees = () => axios.get(API_URL);
-export const getEmployeeById = (id) => axios.get(`${API_URL}/${id}`);
 export const addEmployee = (employee) => axios.post(API_URL, employee);
 export const updateEmployee = (id, employee) => axios.put(`${API_URL}/${id}`, employee);
-export const patchEmployee = (id, employee) => axios.patch(`${API_URL}/${id}`, employee);
+export const patchEmployee = (id, data) => axios.patch(`${API_URL}/${id}`, data);
 export const deleteEmployee = (id) => axios.delete(`${API_URL}/${id}`);
